70ee6a1170b901666c195159e5607094cd9e48fc
Andrew Summers <andrew.z.summers@gmail.com>
2016-03-23 10:13:44 -0500

    initial commit

---
+++ atools/monolayer.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+class Chain(object):
+    """ """
+    def __init__(self,types,charges,xyz):
+
+class Monolayer(object):
+    """ """
+    def __init__(self,chains):
+    
+    def n_chains(self):
