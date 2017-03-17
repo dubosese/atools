35906ce5886c991292e0f0d1b841f7f807dde8e4
Andrew Summers <summeraz@master.cl.vanderbilt.edu>
2017-03-17 09:56:42 -0500

    Add function for calculating nematic order

--- atools/structure.py
+++ atools/structure.py
@@ -4,16 +4,37 @@ import numpy as np
 
 import mdtraj as md
 
-def calc_nematic_order(traj_filename, top_filename, output_filename):
-    first = md.load_frame(traj_filename, 0, top=top_filename)
-    atoms = list(first.top.atoms)
+def calc_nematic_order(traj_filename, top_filename, output_filename,
+                       n_chains):
+    """Calculate the nematic order of a monolayer
 
-    count = 0
-    chain_indices_bot = []
-    chain_indices_top = []
-    for i, atom in enumerate(atoms):
-        if atom.name == ""
+    Returns the average nematic order at each frame of a trajectory
+    between the top and bottom monolayers in a dual monolayer system.
 
+    Notes
+    -----
+    Assumes a unique chain prototype.
+    Assumes identical top and bottom monolayers.
+
+    """
+    topology = md.load(top_filename).topology
+    atoms = np.array(list(topology.atoms))
+    atom_names = [atom.name for atom in atoms]
+    monolayer_begins = atom_names.index('C')
+    half_atoms = int(len(atoms)/2)
+    backfill_sites = int(atom_names.count('OS')/2 - n_chains)
+    bottom_monolayer = [atom.index for atom in atoms[monolayer_begins:half_atoms - backfill_sites]]
+    top_monolayer = [atom.index for atom in atoms[monolayer_begins + half_atoms:-backfill_sites]]
+    bottom_chains = np.array_split(bottom_monolayer, n_chains)
+    top_chains = np.array_split(top_monolayer, n_chains)
+    bottom_chains_list = [chain.tolist() for chain in bottom_chains]
+    top_chains_list = [chain.tolist() for chain in top_chains]
+
+    traj = md.load(traj_filename, top=top_filename)
+    S2_bottom = md.compute_nematic_order(traj, indices=bottom_chains_list)
+    S2_top = md.compute_nematic_order(traj, indices=top_chains_list)
+    S2_mean = np.mean([S2_bottom, S2_top], axis=0)
+    np.savetxt(output_filename, np.column_stack((traj.time, S2_mean)))
 
 def identify_rigid_groups(monolayer, terminal_group=None, freeze_thickness=5):
     bounding_box = monolayer.boundingbox
