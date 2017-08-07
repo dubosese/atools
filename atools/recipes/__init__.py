bb2bddd3237b075442270f7f505c0bdcd0993909
Andrew Z. Summers <andrew.z.summers@gmail.com>
2017-08-07 09:23:48 -0500

    Add tip recipe to init

--- atools/recipes/__init__.py
+++ atools/recipes/__init__.py
@@ -1,4 +1,5 @@
 from atools.recipes.dual_surface import DualSurface
 from atools.recipes.monolayer import Monolayer
 from atools.recipes.silica_interface import SilicaInterface
+from atools.recipes.silica_tip import SilicaTip
 from atools.recipes.surface_monolayer import SurfaceMonolayer
